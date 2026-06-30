e0f4c67bb8c8c81fd4a651d2301442d02f85ed7d
Campbelle-mac <campbelle@macstudent.com.au>
2026-06-30 12:14:07 +1000

    Dreamweaver push

--- script.js
+++ script.js
@@ -1,1 +1,13 @@
 console.log("JavaScript Working");
+
+const mainBar = document.getElementById("mainBarID");
+
+mainBarID.addEventListener("mouseover", function () {
+	mainBarID.style.backgroundColor = "red";
+})
+
+mainBarID.addEventListener("mouseout", function () {
+	mainBarID.style.backgroundColor = "blue";
+})
+
+
