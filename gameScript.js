74e78180a123d222eedcd2d9a53501598b06100f
Campbelle-mac <campbelle@macstudent.com.au>
2026-07-03 10:39:04 +1000

    working tools

--- gameScript.js
+++ gameScript.js
@@ -2,23 +2,41 @@ console.log("JavaScript Working");
 
 const page = document.getElementById("gameBody");
 const cords = document.getElementById("cords");
+const slider = document.getElementById("drawSlider");
 
 let isDrawing = false;
+let drawType = "square";
+let drawSize = 50;
+
+slider.oninput = function() {
+	drawSize = this.value;
+}
+
+function squareTool() {
+	drawType = "square";
+}
+
+function circleTool() {
+	drawType = "circle";
+}
 
 function drawBox() {
 	let X = event.offsetX;
 	let Y = event.offsetY;
 	const box = document.createElement("div");
-	let widthHeight = 25;
-	
 	box.className = "box";
 	box.style.pointerEvents = "none";
 	box.style.position = "absolute";
-	box.style.top = Y - 20 + (widthHeight / 2) + "px";
-	box.style.left = X + 84 + (widthHeight / 2) + "px";
-	box.style.width = widthHeight + "px";
-	box.style.height = widthHeight + "px";
+	box.style.top = Y + 125 - (drawSize / 2) + "px";
+	box.style.left = X + 100 - (drawSize / 2) + "px";
+	box.style.width = drawSize + "px";
+	box.style.height = drawSize + "px";
 	box.style.backgroundColor = "black";
+	if (drawType == "circle") {
+		box.style.borderRadius = "100%";
+	} else if (drawType == "square") {
+		box.style.borderRadius = "0%";
+	}
 	page.append(box);
 }
 
